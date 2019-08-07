02af605a6eb3ed5fcab93792b7dff4aab3e69686
Morgan <msandler8@gmail.com>
2019-08-07 17:28:15 -0400

    adding a generic APICall object

---
+++ app/src/main/java/com/spartahack/spartahack_android/tests/APICall.kt
@@ -0,0 +1,18 @@
+package com.spartahack.spartahack_android.tests
+
+class APICall {
+
+    var path : String
+
+    constructor(path: String){
+        this.path = path
+    }
+
+}
+
+
+fun main(){
+    APICall("test")
+
+
+}
